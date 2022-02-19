af0bac8b6154f1112977dd03ad3ad591fa294641
Jason Park <binarydiver@outlook.com>
2022-02-19 15:25:12 +0900

    modify location to install vim plug

--- mac_os/install-vim.zsh
+++ mac_os/install-vim.zsh
@@ -4,5 +4,5 @@
 cp .vimrc ~/
 
 # Install Vim Plug.
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
