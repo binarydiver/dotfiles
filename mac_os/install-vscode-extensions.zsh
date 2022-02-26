edecbc0e2ef3c42cdb1b92fcc1478443b2c93507
Jason Park <binarydiver@outlook.com>
2022-02-26 16:47:37 +0900

    add script to install vscode extension

---
+++ mac_os/install-vscode-extensions.zsh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+# angular
+code --install-extension angular.ng-template
+code --install-extension infinity1207.angular2-switcher
+
+# devops
+code --install-extension azuredevspaces.azds
+code --install-extension mindaro.mindaro
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-aks-tools
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-vscode-deploy-azure.azure-deploy
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension visualstudioonlineapplicationinsights.application-insights
+
+# fun
+code --install-extension vscode-icons-team.vscode-icons
+
+# git
+code --install-extension eamodio.gitlens
+
+# lint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension docsmsft.docs-authoring-pack
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+
+# utilities
+code --install-extension gruntfuggly.todo-tree
+code --install-extension MS-vsliveshare.vsliveshare
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension chrisbibby.hide-node-modules
+
+# web
+code --install-extension ms-edgedevtools.vscode-edge-devtools
