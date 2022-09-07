119fed7f56408e5e9d2344fc4cadffc322438317
Jason Park <binarydiver@outlook.com>
2022-09-07 22:28:31 +0900

    add keybinding for vscode

--- vscode/keybindings.js
+++ vscode/keybindings.js
@@ -2,5 +2,10 @@
   {
     "key": "ctrl+m",
     "command": "workbench.action.toggleMaximizedPanel"
+  },
+  {
+    "key": "ctrl+k",
+    "command": "workbench.debug.panel.action.clearReplAction",
+    "when": "inDebugRepl"
   }
 ]
