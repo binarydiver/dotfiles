b442bebf35c54212f1e5d17460a7d30213b05319
Jason Park <binarydiver@outlook.com>
2022-11-06 17:39:43 +0900

    update command to install homebrew

--- mac_os/homebrew/install-homebrew.sh
+++ mac_os/homebrew/install-homebrew.sh
@@ -5,10 +5,9 @@ if [[ $(which brew) != "" ]] then
   echo "--- Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
-  if [[ "$(uname)" = "Darwin" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif [[ "$(expr substr $(uname -s) 1 5)" = "Linux" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  if [[ "$(uname)" = "Darwin" ]] || [[ "$(expr substr $(uname -s) 1 5)" = "Linux" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
   else
     echo "--- Not support OS."
   fi
