c6b53eb15824e6f2071b3e0c2ee76f693326b4f0
Jason Park <binarydiver@outlook.com>
2022-11-07 15:38:24 +0900

    update script to install nvm

--- mac_os/homebrew/install-nvm.sh
+++ mac_os/homebrew/install-nvm.sh
@@ -6,12 +6,12 @@ brew install nvm
 echo "--- Add setting to \".zprofile\"."
 mkdir ~/.nvm
 
-echo "export NVM_DIR=\"$HOME/.nvm\"
+echo -e "\n# NVM\nexport NVM_DIR=\"$HOME/.nvm\"
   [ -s \"/opt/homebrew/opt/nvm/nvm.sh\" ] && \. \"/opt/homebrew/opt/nvm/nvm.sh\"
   [ -s \"/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm\" ] && \. \"/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm\"" >> ~/.zprofile
 
 source ~/.zprofile
 
 echo "--- Install specific node version."
-nvm install 14
-nvm alias default 14
+nvm install 16
+nvm alias default 16
