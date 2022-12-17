780bc09b1aeeff71f22b5c76e691536dd2cbd442
Jason Park <binarydiver@outlook.com>
2022-12-17 10:00:37 +0900

    add plugin for dart to vscode-extension

--- mac_os/install-vscode-extensions.zsh
+++ mac_os/install-vscode-extensions.zsh
@@ -1,22 +1,22 @@
 #!/bin/zsh
 
 # angular
-code --install-extension angular.ng-template
-code --install-extension infinity1207.angular2-switcher
+code --install-extension Angular.ng-template
+# code --install-extension infinity1207.angular2-switcher
 
 # devops
-code --install-extension azuredevspaces.azds
-code --install-extension mindaro.mindaro
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-kubernetes-tools.vscode-aks-tools
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-vscode-deploy-azure.azure-deploy
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode.vscode-node-azure-pack
-code --install-extension visualstudioonlineapplicationinsights.application-insights
+# code --install-extension azuredevspaces.azds
+# code --install-extension mindaro.mindaro
+# code --install-extension ms-azuretools.vscode-docker
+# code --install-extension ms-kubernetes-tools.vscode-aks-tools
+# code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+# code --install-extension ms-vscode-deploy-azure.azure-deploy
+# code --install-extension ms-vscode-remote.remote-containers
+# code --install-extension ms-vscode.vscode-node-azure-pack
+# code --install-extension visualstudioonlineapplicationinsights.application-insights
 
 # theme
-code --install-extension vscode-icons-team.vscode-icons
+code --install-extension PKief.material-icon-theme
 code --install-extension GitHub.github-vscode-theme
 
 # git
@@ -26,14 +26,18 @@ code --install-extension eamodio.gitlens
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension docsmsft.docs-authoring-pack
 code --install-extension esbenp.prettier-vscode
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 
 # utilities
 code --install-extension gruntfuggly.todo-tree
-code --install-extension MS-vsliveshare.vsliveshare
+# code --install-extension MS-vsliveshare.vsliveshare
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
 code --install-extension VisualStudioExptTeam.vscodeintellicode-completions
 
 # web
-code --install-extension ms-edgedevtools.vscode-edge-devtools
+# code --install-extension ms-edgedevtools.vscode-edge-devtools
+
+# flutter
+code --install-extension Dart-Code.dart-code
+code --install-extension Dart-Code.flutter
