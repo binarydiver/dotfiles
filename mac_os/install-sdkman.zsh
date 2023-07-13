78ff1a7015e62a0cd5b1059546660fe5d413a41e
Jason Park <binarydiver@outlook.com>
2023-07-13 09:50:44 +0900

    add script to install java

---
+++ mac_os/install-sdkman.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# ref. https://sdkman.io/install
+curl -s "https://get.sdkman.io" | bash
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk version
+
+# Install Java
+sdk install java 8.0.362-amzn
+sdk install java 11.0.18-ms
